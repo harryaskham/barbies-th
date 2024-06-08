83512d11d89d7ef2e502653a494cd4ef9f232ab4
Harry Askham <harryaskham@gmail.com>
2024-06-08 10:55:25 +0100

    flakify

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "barbies-th";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; config = { allowBroken = true; }; };
+        packageName = "barbies-th";
+        haskellPackages = pkgs.haskell.packages.ghc981;
+        bin = haskellPackages.callCabal2nix "${packageName}" ./. { };
+      in with pkgs; rec {
+        packages.${packageName} = bin;
+        defaultPackage = packages.${packageName};
+      }
+    );
+}
